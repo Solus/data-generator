6b232f91671a5f7059583497c8af69d9e6ef91e0
alter <mberic@gmail.com>
2025-08-17 15:31:57 +0200

    Turned off static export

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Configure the project for static export
-  output: 'export',
-
-  // The directory where the exported site will be generated
-  distDir: 'docs',
-
-  // The base path for the project on GitHub Pages
-  basePath: '/data-generator',
-
-  // The asset prefix to ensure assets are loaded correctly from the sub-path
-  assetPrefix: '/data-generator/',
-
-  // It's a good practice to disable image optimization for static exports if not using a custom loader
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
